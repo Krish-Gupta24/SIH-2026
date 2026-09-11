ff992948c15209412b1d8e54515ef490e363feef
Krish Gupta <krishg053@gmail.com>
2026-09-11 18:40:43 +0530

    backend connected

--- database/migrations/env.py
+++ database/migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
-from backend.core.config import settings
+from backend.core.database import engine
 from backend.models import Base
 
 config = context.config
@@ -16,11 +16,10 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 
 def run_migrations_offline() -> None:
-    url = config.get_main_option("sqlalchemy.url")
+    url = str(engine.url)
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -39,16 +38,8 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_async_migrations() -> None:
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    async with connectable.connect() as connection:
+    async with engine.connect() as connection:
         await connection.run_sync(do_run_migrations)
-
-    await connectable.dispose()
 
 
 def run_migrations_online() -> None:
